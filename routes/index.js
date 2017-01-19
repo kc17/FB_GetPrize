21d6b4c649d93b96914b1a8b9bd35873edcd84b7
polo13999 <polo13999@gmail.com>
2017-01-20 05:07:08 +0800

    change code

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var request = require('request');
 /* GET home page. */
 //router.get('/', function (req, res, next) {
 //res.render('index', { title: 'Express' });
-
-request('https://graph.facebook.com/v2.8/1782686801973742_1865749533667468/comments?limit=100&access_token=EAACEdEose0cBAHeqPArAJEp00traDoEfyPuf9SWw6a6mFtSL9T8FxQi5jklVaznAZAWKze7ZBQn25sa9F4ahk4rnzBecNP6gwPqC5ZAwGRFcZC9RyUjYZA0UxBgQoVlwqNH8ZAkpHl77J243Ebo9UzmZBTrSchfV8d4p3z07UcSMgZDZD', 'utf-8', function (res, rep, data) {
+var access_token = "";
+request(`https://graph.facebook.com/v2.8/1782686801973742_1865749533667468/comments?limit=100&access_token=${access_token}`, 'utf-8', function (res, rep, data) {
   var data1 = JSON.parse(data);
 
   for (i = 0; i < 1; i++) {
